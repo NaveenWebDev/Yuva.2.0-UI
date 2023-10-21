79c3458f0bd71add557db0078ea9b7b199f2ef8c
NaveenWebDev <ns222280@gmail.com>
2023-10-21 16:36:12 +0530

    add some changes in univercity section

--- src/Components/Universities.js
+++ src/Components/Universities.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from 'react';
 import satyaIcon from "../assests/satmew-jayte-logo-FBBEBAEA31-seeklogo 2.svg";
-import "../Css/univercity.css"
-import arrow from "../assests/Vector.svg"
-import unione from "../assests/univercity/uniOne.svg"
-import uniTwo from "../assests/univercity/uniTwo.svg"
-import uniThree from "../assests/univercity/uniThree.svg"
-import uniFour from "../assests/univercity/uniFour.svg"
-import uniFive from "../assests/univercity/uniFive.svg"
-import uniSix from "../assests/univercity/uniSix.svg"
+import "../Css/univercity.css";
+import arrow from "../assests/Vector.svg";
+import unione from "../assests/univercity/uniOne.svg";
+import uniTwo from "../assests/univercity/uniTwo.svg";
+import uniThree from "../assests/univercity/uniThree.svg";
+import uniFour from "../assests/univercity/uniFour.svg";
+import uniFive from "../assests/univercity/uniFive.svg";
+import uniSix from "../assests/univercity/uniSix.svg";
+import satyaimg from "../assests/univercity/satmew-jayte-logo-FBBEBAEA31-seeklogo 1.svg";
+import unaUnivlogo from "../assests/univercity/uvaUnivlogo.svg";
+
 
 const Universities = () => {
   return (
     <>
-        <div className="container satya">
+        <div className="container satya position-relative">
+        <img src={satyaimg} alt="logo" id='satyaimg' className='position-absolute' />
+        <img src={unaUnivlogo} alt="logo" id='unaUni' className='position-absolute' />
+
             <h2 className='text-center fw-bolder mb-5'>We are supported by more than 20 ministries and universities</h2>
 
             <div className="univercityIcons">
